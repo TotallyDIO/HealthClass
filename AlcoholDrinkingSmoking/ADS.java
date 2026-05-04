52655916df575bc841ba0955a35259af884ed460
TotallyDIO <totallydio@proton.me>
2026-05-04 11:47:07 -0500

    first run

--- AlcoholDrinkingSmoking/ADS.java
+++ AlcoholDrinkingSmoking/ADS.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;  // Reads inputs
 
 public class ADS{
-    static int totalQuestions = 40
-    int currentQuestion = 1
-    int correctAnswers = 0
-    int incorrectAnswers = 0
+    static int totalQuestions = 40;
+    int currentQuestion = 1;
+    int correctAnswers = 0;
+    int incorrectAnswers = 0;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Creates Scanner
 
         // Introduction
         System.out.println("Welcome to the Alcohol, Drinking, and Smoking Quiz!");
         System.out.println("You will be asked " + totalQuestions + " questions. Good luck!");
-
-        
-
+        System.out.println("Press Enter to start...");
+        String a = scanner.nextLine(); // Waits for user to press Enter
+        if (a.isEmpty()) {
+            System.out.println("Starting quiz...");
+            System.out.println("\033[H\033[2J"); // Clears the console
+        }
     }
 }
