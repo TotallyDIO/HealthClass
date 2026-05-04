ca387b096208992786c225028f1ae1acce6b8b16
TotallyDIO <hunter.tadlock911@gmail.com>
2026-05-04 11:35:09 -0500

    initial commit

--- AlcoholDrinkingSmoking/ADS.java
+++ AlcoholDrinkingSmoking/ADS.java
@@ -1,5 +1,12 @@
-
+import java.util.Scanner;  // Reads inputs
 
 public class ADS{
+    static int totalQuestions = 40
+    int currentQuestion = 1
+    int correctAnswers = 0
+    int incorrectAnswers = 0
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+    }
 }
