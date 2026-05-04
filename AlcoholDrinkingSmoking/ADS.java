4786fbb9a0fdaeeae4866ba6c95dc6c6a4c2c44f
TotallyDIO <totallydio@proton.me>
2026-05-04 11:39:14 -0500

    moved IS and made ant work

--- AlcoholDrinkingSmoking/ADS.java
+++ AlcoholDrinkingSmoking/ADS.java
@@ -6,7 +6,13 @@ public class ADS{
     int correctAnswers = 0
     int incorrectAnswers = 0
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Creates Scanner
+
+        // Introduction
+        System.out.println("Welcome to the Alcohol, Drinking, and Smoking Quiz!");
+        System.out.println("You will be asked " + totalQuestions + " questions. Good luck!");
+
+        
 
     }
 }
